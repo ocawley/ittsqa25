56db5c3a94d548d7fe812dbda13cc7292a963f6d
AdamLee123 <X00088467@gmail.com>
2014-10-16 10:49:14 +0100

    fjaskljaskl;fjkl;asfjkl;asd

---
+++ X00088467-grades.sh
@@ -0,0 +1,35 @@
+#Test Case 1
+#Inputs: 40 40
+#Expected Output: Fail
+#Observed Output: Fail
+java grades 40 40
+
+#Test Case 2
+#Inputs: 60 40
+#Expected Output: Pass
+#Observed Output: Fail
+java grades 60 40
+
+#Test Case 3
+#Inputs: 99 10
+#Expected Output: Component Fail
+#Observed Output: Component Fail
+java grades 99 10
+
+#Test Case 4
+#Inputs: 10 60
+#Expected Output: Component Fail
+#Observed Output: Component Fail
+java grades 10 60
+
+#Test Case 5
+#Inputs: 80 100
+#Expected Output: Pass with distinction
+#Observed Output: Pass with distinction
+java grades 80 100
+
+#Test Case 6
+#Inputs: 101 102
+#Expected Output: Invalid input
+#Observed Output: Invalid input
+java grades 101 102
